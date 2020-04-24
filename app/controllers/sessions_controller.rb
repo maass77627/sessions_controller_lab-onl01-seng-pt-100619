2bcacde248e0334553d9dc47cb907555ad5e22fe
kelly r maass <kmaass77627@gmail.com>
2020-04-24 09:02:29 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,6 @@ class SessionsController < ApplicationController
         redirect_to '/'
       session[:name] = name_from_params
       redirect_to root_path
-    else
-      redirect_to login_path
     end
   end
 
