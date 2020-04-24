47171fced75c576c1216ef7d7bf5ec5ffe30cfc8
kelly r maass <kmaass77627@gmail.com>
2020-04-24 08:27:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new 
   end
 
   def create
- if params[:name].nil?
+    if params[:name].nil?
     redirect_to 'new'
   else
     session[:name] = params[:name]
     redirect_to '/'
   end
 end
-end
 
-def destroy
-  if session[:name]
-session[:name].delete
-end
-end
-end
-
-def new
-   @user = User.new
- end
- def create
-   user = User.find_by(name: params[:name])
-   if user.save
-     user.id = session[:user_id]
-   else
-     flash[:notice] = "Form is invalid"
-     flash[:color]= "invalid"
+ def destroy
+   if session[:name]
+    session[:name].delete
    end
-   render "new"
  end
 end
+
